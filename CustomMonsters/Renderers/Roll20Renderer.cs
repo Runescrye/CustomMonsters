4dfcb2e04538fc020b0c2b049f26d87f5e888f02
Runescrye <tzah.levi@gmail.com>
2021-06-06 23:54:58 +0300

    Update Roll20Renderer.cs
    Rolls without modifiers should now be picked up as damage rolls

--- CustomMonsters/Renderers/Roll20Renderer.cs
+++ CustomMonsters/Renderers/Roll20Renderer.cs
@@ -122,7 +122,7 @@ namespace MonsterPorter.Renderers
             }
         }
 
-        private static Regex dmgRegex = new Regex("\\d?d\\d+\\s*([+-]\\s*\\d+)");
+        private static Regex dmgRegex = new Regex("\\d?d\\d+\\s*([+-]\\s*\\d+)?");
         private string AnnotateDamage(string source)
         {
             return dmgRegex.Replace(source, x => "XX" + x.Value + "ZZ");
